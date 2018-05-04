0807efbbe8c828617da78a6a6408ce41ec88e9dd
japanoise <japanoise@tfwno.gf>
2018-05-04 11:35:48 +0100

    Test redo and some undo edge cases

--- undo_test.go
+++ undo_test.go
@@ -6,6 +6,12 @@ import (
 	"testing"
 )
 
+func (b *EditorBuffer) FailIfBufferNe(lines []string, t *testing.T) {
+	if !b.TestIs(lines) {
+		t.Error("Expected buffer", lines, "but was actually:\n", b.PrintBuf())
+	}
+}
+
 func (b *EditorBuffer) PrintBuf() string {
 	buf := bytes.Buffer{}
 	for i, row := range b.Rows {
@@ -17,13 +23,27 @@ func (b *EditorBuffer) PrintBuf() string {
 	return buf.String()
 }
 
+func (b *EditorBuffer) TestIs(lines []string) bool {
+	// Special case - one empty line is functionally equivalent to an empty buffer
+	if len(lines) == 0 && (b.LinesNum() == 1 && b.Rows[0].Data == "") {
+		return true
+	}
+	if len(lines) != len(b.Rows) {
+		return false
+	}
+	for i, line := range lines {
+		if b.Rows[i].Data != line {
+			return false
+		}
+	}
+	return true
+}
+
 func TestAppendUndo(t *testing.T) {
 	InitEditor()
 	editorInsertStr("test")
 	editorUndoAction()
-	if Global.CurrentB.Rows[0].Data != "" {
-		t.Error("expected empty buffer, but buffer's content is:\n", Global.CurrentB.PrintBuf())
-	}
+	Global.CurrentB.FailIfBufferNe([]string{}, t)
 }
 
 func TestDeleteUndo(t *testing.T) {
@@ -31,9 +51,7 @@ func TestDeleteUndo(t *testing.T) {
 	editorInsertStr("test")
 	editorDelChar()
 	editorUndoAction()
-	if Global.CurrentB.Rows[0].Data != "test" {
-		t.Error("expected buffer's content to be \"test\", but buffer's content is:\n", Global.CurrentB.PrintBuf())
-	}
+	Global.CurrentB.FailIfBufferNe([]string{"test"}, t)
 }
 
 func TestMultilineInsertUndo(t *testing.T) {
@@ -45,9 +63,7 @@ func TestMultilineInsertUndo(t *testing.T) {
 	editorInsertStr("test3")
 	editorInsertNewline(false)
 	editorUndoAction()
-	if Global.CurrentB.LinesNum() != 1 || Global.CurrentB.Rows[0].Data != "" {
-		t.Error("expected empty buffer, but buffer's content is:\n", Global.CurrentB.PrintBuf())
-	}
+	Global.CurrentB.FailIfBufferNe([]string{}, t)
 }
 
 func TestMultilineDeleteUndo(t *testing.T) {
@@ -62,7 +78,79 @@ func TestMultilineDeleteUndo(t *testing.T) {
 	Global.CurrentB.cy = 0
 	doKillRegion()
 	editorUndoAction()
-	if Global.CurrentB.LinesNum() != 3 || Global.CurrentB.Rows[0].Data != "test1" || Global.CurrentB.Rows[1].Data != "test2" || Global.CurrentB.Rows[2].Data != "test3" {
-		t.Error("expected buffer {\"test1\",\"test2\",\"test3\"}, but buffer's content is:\n", Global.CurrentB.PrintBuf())
+	Global.CurrentB.FailIfBufferNe([]string{"test1", "test2", "test3"}, t)
+}
+
+func TestLastLineUndo(t *testing.T) {
+	InitEditor()
+	editorInsertStr("test")
+	Global.CurrentB.cx = 0
+	Global.CurrentB.cy = 0
+	editorInsertStr("a ")
+	Global.CurrentB.cy = Global.CurrentB.NumRows
+	Global.CurrentB.cx = 0
+	editorInsertStr("foo")
+	editorUndoAction()
+	Global.CurrentB.FailIfBufferNe([]string{"a test", ""}, t)
+}
+
+func TestDoUndoWithNothing(t *testing.T) {
+	InitEditor()
+	editorUndoAction() // If this doesn't crash, then test for a message
+	if Global.Input == "" {
+		t.Error("No message to the user was printed")
 	}
+}
+
+func TestDoRedoWithNothing(t *testing.T) {
+	InitEditor()
+	doOneRedo(nil) // If this doesn't crash, then test for a message
+	if Global.Input == "" {
+		t.Error("No message to the user was printed")
+	}
+}
+
+func TestAppendRedo(t *testing.T) {
+	InitEditor()
+	editorInsertStr("test")
+	editorUndoAction()
+	doOneRedo(nil)
+	Global.CurrentB.FailIfBufferNe([]string{"test"}, t)
+}
+
+func TestDeleteRedo(t *testing.T) {
+	InitEditor()
+	editorInsertStr("test")
+	editorDelChar()
+	editorUndoAction()
+	doOneRedo(nil)
+	Global.CurrentB.FailIfBufferNe([]string{"tes"}, t)
+}
+
+func TestMultilineInsertRedo(t *testing.T) {
+	InitEditor()
+	editorInsertStr("test1")
+	editorInsertNewline(false)
+	editorInsertStr("test2")
+	editorInsertNewline(false)
+	editorInsertStr("test3")
+	editorUndoAction()
+	doOneRedo(nil)
+	Global.CurrentB.FailIfBufferNe([]string{"test1", "test2", "test3"}, t)
+}
+
+func TestMultilineDeleteRedo(t *testing.T) {
+	InitEditor()
+	editorInsertStr("test1")
+	editorInsertNewline(false)
+	editorInsertStr("test2")
+	editorInsertNewline(false)
+	editorInsertStr("test3")
+	setMark(Global.CurrentB)
+	Global.CurrentB.cx = 0
+	Global.CurrentB.cy = 0
+	doKillRegion()
+	editorUndoAction()
+	doOneRedo(nil)
+	Global.CurrentB.FailIfBufferNe([]string{}, t)
 }
